7d462a9c1af732c885a8d1567fceb921bc032dfa
Manasi Shah <manasishah@Manasis-MacBook-Pro.local>
2015-01-28 12:53:29 -0800

    Initial Commit

---
+++ GifImageDisplay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GifImageDisplay
+//
+//  Created by Manasi Shah on 28/01/15.
+//  Copyright (c) 2015 Manasi Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
